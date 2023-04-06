8390dc538ff4b80b428cf389b91477a2c3a897e8
Makmakkerti <makmakkerti007@gmail.com>
2023-04-05 19:21:14 -0600

    get weather by city name added

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("", async (req, res, next) => {
+  const { city = "London" } = req.query;
   try {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=edmonton&appid=${process.env.OPEN_WEATHER_MAP_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_MAP_KEY}`
     );
     res.json(data);
   } catch (error) {
-    error.message = `Weather service connection error: ${error.message}`;
+    error.message = `Weather service error: ${error.message}`;
     return next(error);
   }
 });
